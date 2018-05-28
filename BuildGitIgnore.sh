1a5f553c61a73310bc9922b19fe44be197ac6fdf
= <=>
2018-05-29 01:56:10 +0800

    添加复制把.gitignore 复制到其他的脚本

---
+++ BuildGitIgnore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#复制文件
+#"/Users/stephanie/Desktop/SourceTree/ConfigureFiles/.gitignore"
+
+Build_Git_IgnoreFile_Path="/Users/stephanie/Desktop/SourceTree/ConfigureFiles/.gitignore"
+
+cp -R $Build_Git_IgnoreFile_Path "$PROJECT_DIR"
