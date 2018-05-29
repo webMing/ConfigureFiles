2e7273fa031f6b839f98408a032907a9ea07d9c8
= <=>
2018-05-29 22:01:09 +0800

    脚本文件修改.

--- BuildGitIgnore.sh
+++ BuildGitIgnore.sh
@@ -2,6 +2,10 @@
 #复制文件
 #"/Users/stephanie/Desktop/SourceTree/ConfigureFiles/.gitignore"
 
-Build_Git_IgnoreFile_Path="/Users/stephanie/Desktop/SourceTree/ConfigureFiles/.gitignore"
+#切换到当前工作目录
+#cd "$PROJECT_DIR" 工作在当前目录不用切换目录,将.gitignore 改为不隐藏方便以后要对其进行管理;
+#创建.gitginore快键键的方式Command + F
 
-cp -R $Build_Git_IgnoreFile_Path "$PROJECT_DIR"
+Build_Git_IgnoreFile_Path="/Users/stephanie/Desktop/SourceTree/ConfigureFiles/gitignore"
+#gitignore文件移到当前工作目录
+cp -R $Build_Git_IgnoreFile_Path ./.gitignore
