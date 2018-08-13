5323764aa71326e39655fe549cb6157ae4075719
= <=>
2018-08-13 14:42:13 +0800

    修改创建.ignore 文件脚本的快捷键

--- BuildGitIgnore.sh
+++ BuildGitIgnore.sh
@@ -4,7 +4,7 @@
 
 #切换到当前工作目录
 #cd "$PROJECT_DIR" 工作在当前目录不用切换目录,将.gitignore 改为不隐藏方便以后要对其进行管理;
-#创建.gitginore快键键的方式Command + F
+#创建.gitginore快键键的方式 Command + option + o
 
 Build_Git_IgnoreFile_Path="/Users/stephanie/Desktop/SourceTree/ConfigureFiles/gitignore"
 #gitignore文件移到当前工作目录
